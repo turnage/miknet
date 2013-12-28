93865688930b238aa8c5291c6a24d32cd76aa9d5
Payton Turnage <paytonturnage@gmail.com>
2013-12-28 04:18:42 +0000

    Update the tutorial file for vectors.
    The wiki will need to updated accordingly.

--- servtest.c
+++ servtest.c
@@ -25,42 +25,42 @@ int main (int argc, char **argv)
 	while (!quit) {
 
 		/* Let the node execute queued commands and collect
-		   incoming packets. Provide it a ~maximum blocking
+		   incoming event->. Provide it a ~maximum blocking
 		   time in milliseconds. */
 		miknode_poll(&node, 100);
 
-		/* After each call, node.packs will be a linked list
-		   of events that came in during the poll. */
-		while (node.packs) {
+		/* After a call to miknode_poll, fetch events from mikevent
+                   until there are no more events to handle. Make sure to
+                   handle all events before polling again, or those events will
+                   be lost. */
 
-			mikpack_t packet = node.packs->pack;
-			
-			/* Packet types
+		mikpack_t *event = mikevent(&node);
+
+		while (event) {
+
+			/* Event types
 				MIK_JOIN: A new peer joined; data field is NULL.
 				MIK_QUIT: A peer quit; data field is NULL.
 				MIK_DATA: A peer sent data; data field is set. 
 			*/
 
-			/* Miknet packets can be sent over virtual channels,
+			/* Miknet event-> can be sent over virtual channels,
 			   market by an unsigned 32 bit integer. The default
 			   channel (for join/quit notifications) is 0. */
-			printf("Packet on channel %u; ", packet.channel);
+			printf("Packet on channel %u; ", event->channel);
 
-			if (packet.type == MIK_JOIN) {
-				printf("New peer in slot %d.\n", packet.peer);
-			} else if (packet.type == MIK_QUIT) {
-				printf("Lost peer in slot %d.\n", packet.peer);
-			} else if (packet.type == MIK_DATA) {
-				printf("Data from peer: %s\n", packet.data);
+			if (event->type == MIK_JOIN) {
+				printf("New peer in slot %d.\n", event->peer);
+			} else if (event->type == MIK_QUIT) {
+				printf("Lost peer in slot %d.\n", event->peer);
+			} else if (event->type == MIK_DATA) {
+				printf("Data from peer: %s\n", event->data);
 
-				if (!strncmp(packet.data, "quit\0", 5))
+				if (!strncmp(event->data, "quit\0", 5))
 					quit = 1;
 			}
 
-			/* Use miklist_next() to step forward in the packs
-			   linked list; it will properly handle freeing the
-			   memory used by the packets. */
-			node.packs = miklist_next(node.packs);
+			event = mikevent(&node);
 		}
 	}
 
