17fc2864f3037b1425b60a80cfef2ea5f9c1a2f4
Payton Turnage <paytonturnage@gmail.com>
2014-07-30 00:00:51 -0600

    Document mikid()

--- src/include/miknet/mikid.h
+++ src/include/miknet/mikid.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 
+/**
+ *  Space: Internal only.
+ *
+ *  Returns an identifier. This identifier is guarunteed not to have been given
+ *  to any preceding caller. No future callers will receive it either.
+ */
 uint64_t mikid();
 
 #endif  /* MIKNET_MIKID_H_ */
