00e6a5d7a13b46de9e1b421c81ccb29c161f6d3b
Payton Turnage <paytonturnage@gmail.com>
2015-03-20 18:48:07 -0400

    Reorganizes testing of mikaddr.
    From now unit tests will be "unit tests" rather than "path tests" because the
    setup is so redundant when separating tests based on paths.

--- tests/mikaddr_test.c
+++ tests/mikaddr_test.c
@@ -13,81 +13,54 @@ START_TEST(test_create)
 	struct sockaddr_in *expected;
 	struct sockaddr_in *actual;
 	posix_mock_t mock;
-	int status;
 
 	expected_addr.ai_addr = &expected_sockaddr;
 	expected = (struct sockaddr_in *)expected_addr.ai_addr;
-
 	mock.posix = mikposixmock();
-	mock.getaddrinfo_return = MIKERR_NONE;
 	mock.getaddrinfo_arg_set = &expected_addr;
-	status = mikaddr(&addr, (posix_t *)&mock, "127.0.0.1", 80);
-	actual = (struct sockaddr_in *)&addr.addr;
 
-	ck_assert_int_eq(status, MIKERR_NONE);
+	/* Proper use. */
+	mock.getaddrinfo_return = MIKERR_NONE;
+	ck_assert_int_eq(	mikaddr(&addr, (posix_t *)&mock, "0.0.0.0", 80),
+				MIKERR_NONE);
 	ck_assert_int_eq(addr.addrlen, expected_addr.ai_addrlen);
-	ck_assert_int_eq(expected->sin_port, actual->sin_port);
-	ck_assert_int_eq(expected->sin_addr.s_addr, actual->sin_addr.s_addr);
+	actual = (struct sockaddr_in *)&addr.addr;
+	ck_assert_int_eq(actual->sin_port, expected->sin_port);
+	ck_assert_int_eq(actual->sin_addr.s_addr, expected->sin_addr.s_addr);
 
 	/* NULL address should request INADDR_ANY */
-	status = mikaddr(&addr, (posix_t *)&mock, NULL, 80);
+	ck_assert_int_eq(	mikaddr(&addr, (posix_t *)&mock, NULL, 80),
+				MIKERR_NONE);
 	actual = (struct sockaddr_in *)&addr.addr;
-	ck_assert_int_eq(status, MIKERR_NONE);
 	ck_assert_int_eq(actual->sin_addr.s_addr, INADDR_ANY);
-}
-END_TEST
-
-START_TEST(test_create_sys_fails)
-{
-	mikaddr_t addr;
-	struct addrinfo expected_addr;
-	posix_mock_t mock;
-	int status;
 
 	/* Failure by report. */
 	mock.posix = mikposixmock();
 	mock.getaddrinfo_return = MIKERR_LOOKUP;
-	mock.getaddrinfo_arg_set = &expected_addr;
-	status = mikaddr(&addr, (posix_t *)&mock, "127.0.0.1", 80);
-
-	ck_assert_int_eq(status, MIKERR_LOOKUP);
+	ck_assert_int_eq(	mikaddr(&addr, (posix_t *)&mock, "0.0.0.0", 80),
+				MIKERR_LOOKUP);
 
 	/* Failure by no results. */
 	mock.getaddrinfo_return = MIKERR_NONE;
 	mock.getaddrinfo_arg_set = NULL;
-	status = mikaddr(&addr, (posix_t *)&mock, "127.0.0.1", 80);
+	ck_assert_int_eq(	mikaddr(&addr, (posix_t *)&mock, "0.0.0.0", 80),
+				MIKERR_LOOKUP);
 
-	ck_assert_int_eq(status, MIKERR_LOOKUP);
-}
-END_TEST
-
-START_TEST(test_create_bad_ptr)
-{
-	mikaddr_t addr;
-	posix_mock_t mock;
-	int status;
-
+	/* Bad inputs. */
 	mock.getaddrinfo_return = MIKERR_NONE;
-
-	status = mikaddr(&addr, NULL, "127.0.0.1", 80);
-	ck_assert_int_eq(status, MIKERR_BAD_PTR);
-
-	status = mikaddr(NULL, (posix_t *)&mock, "127.0.0.1", 80);
-	ck_assert_int_eq(status, MIKERR_BAD_PTR);
+	ck_assert_int_eq(mikaddr(&addr, NULL, "0.0.0.0", 80), MIKERR_BAD_PTR);
+	ck_assert_int_eq(	mikaddr(NULL, (posix_t *)&mock, "0.0.0.0", 80),
+				MIKERR_BAD_PTR);
 }
 END_TEST
 
 Suite *mikaddr_suite()
 {
 	Suite *suite = suite_create("mikaddr_suite");
-	TCase *standard_use = tcase_create("mikaddr_standard_use");
-	TCase *incorrect_use = tcase_create("mikaddr_incorrect_use");
+	TCase *mikaddr_units = tcase_create("mikaddr_units");
 
-	tcase_add_test(standard_use, test_create);
-	tcase_add_test(standard_use, test_create_sys_fails);
-	tcase_add_test(incorrect_use, test_create_bad_ptr);
-	suite_add_tcase(suite, standard_use);
-	suite_add_tcase(suite, incorrect_use);
+	tcase_add_test(mikaddr_units, test_create);
+	suite_add_tcase(suite, mikaddr_units);
 
 	return suite;
 }
