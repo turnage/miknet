2a98f24e8dd8882c70f3697c96430d0b370026dd
Payton Turnage <paytonturnage@gmail.com>
2015-03-21 21:47:40 -0400

    Implements a simple sending program using miknet.
    This also functions as a full test of both the library's functionality and its
    ease of use. This file will likely be replaced or rewritten many times as the
    API evolves.

---
+++ examples/simple_send.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "miknet/mikdef.h"
+#include "miknet/mikgram.h"
+#include "miknet/miknode.h"
+
+#define FAIL(x) fprintf(stderr, x); exit(0)
+
+int main(int argc, char **argv)
+{
+	miknode_t *node;
+	mikgram_t gram;
+
+	if (argc != 3)
+		printf("Usage: ./simple_send [address] [port]\n");
+
+	fprintf(stderr, "Sending mikgram to %s:%s.\n", argv[1], argv[2]);
+
+	node = miknode(0, 1);
+	if (node == NULL) {
+		FAIL("Failed to initialize miknet.\n");
+	} else {
+		fprintf(stderr, "Initialized miknet.\n");
+	}
+
+	if (miknode_new_peer(node, argv[1], atol(argv[2])) < 0) {
+		FAIL("Failed to lookup the given address.\n");
+	} else {
+		fprintf(stderr, "Added %s:%s as a peer.\n", argv[1], argv[2]);
+	}
+
+	if (mikgram(&gram, "Hello", 6) != MIKERR_NONE) {
+		FAIL("Failed to create mikgram.\n");
+	} else {
+		fprintf(stderr, "Created mikgram.\n");
+	}
+
+	if (miknode_send(node, 0, &gram) != MIKERR_NONE) {
+		FAIL("Failed to send mikgram.\n");
+	} else {
+		fprintf(stderr, "Sent mikgram.\n");
+	}
+
+	mikgram_close(&gram);
+	miknode_close(node);
+
+	return 0;
+}
